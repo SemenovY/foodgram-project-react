21702c8a2503911786fdf1b779cb3c2943c5af24
Yuriy <info@juriys.ru>
2023-03-21 22:01:54 +0300

    add some feature

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,18 @@
 """Backend URL Configuration."""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
+    # Django проверяет url-адреса сверху вниз,
+    # нам нужно, чтобы Django сначала проверял адреса в приложении users
+    #   path('auth/', include('users.urls')),
+    # Если какой-то URL не обнаружится в приложении users —
+    # Django пойдёт искать его в django.contrib.auth
+    #   path('auth/', include('django.contrib.auth.urls')),
 ]
