722a1023ee9df06286ada3c7053b2e8e164a6a34
Yuriy <info@juriys.ru>
2023-03-24 22:03:15 +0300

    fix usr model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,10 @@
 """User models."""
 # abstract_user/users/models.py
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-User = get_user_model()
+# User = get_user_model()
 
 
 class User(AbstractUser):
@@ -14,34 +14,34 @@ class User(AbstractUser):
         max_length=150,
         blank=False,
         unique=True,
-        required=True,
+        #        required=True,
     )
 
-    username = (
-        models.ForeignKey(
-            User,
-            on_delete=models.CASCADE,
-            unique=True,
-        ),
-    )
+    #    username = (
+    #        models.ForeignKey(
+    #            User,
+    #            on_delete=models.CASCADE,
+    #            unique=True,
+    #        ),
+    #    )
 
     first_name = (
         models.TextField(
             max_length=150,
-            required=True,
+            #           required=True,
         ),
     )
 
     last_name = (
         models.TextField(
             max_length=150,
-            required=True,
+            #          required=True,
         ),
     )
 
     password = (
         models.TextField(
             max_length=150,
-            required=True,
+            #         required=True,
         ),
     )
--- urls.py
+++ urls.py
@@ -6,15 +6,15 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserViewSet, get_jwt_token, signup
 
 router_v1 = DefaultRouter()
-router_v1.register("users", UserViewSet)
+router_v1.register('users', UserViewSet)
 
 auth_patterns = [
-    path("signup/", signup, name="signup"),
-    path("token/", get_jwt_token, name="get_jwt_token"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('signup/', signup, name='signup'),
+    path('token/', get_jwt_token, name='get_jwt_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns = [
-    path("v1/auth/", include(auth_patterns)),
-    path("v1/", include(router_v1.u::rls)),
+    path('v1/auth/', include(auth_patterns)),
+    path('v1/', include(router_v1.urls)),
 ]
