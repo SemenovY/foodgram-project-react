e600bdaf4dacb0ab59934081b316da57561adfc2
Yuriy <info@juriys.ru>
2023-04-14 18:31:40 +0300

    workflow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,13 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    "django-insecure-q@#new-h8lz4w7wp+-ofo7)mr@vpdpq%3t3v85$%j1%u8m_*%r"
-)
+SECRET_KEY = os.getenv("SECRET_KEY", default=get_random_secret_key())
 
-DEBUG = False
+DEBUG = DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="127.0.0.1").split()
 
 INSTALLED_APPS = [
     "django.contrib.admin",
